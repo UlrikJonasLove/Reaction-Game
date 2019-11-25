5280d4304350ddb9c0cf10bd0b45f9b91da7124e
joro1993 <54711342+joro1993@users.noreply.github.com>
2019-11-25 19:04:39 +0100

    Add files via upload

---
+++ ReactionGame/ReactionGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+class ReactionGame
+{
+    static void Main()
+    {
+        Console.WriteLine("Ok, get ready...");
+        Thread.Sleep(2000);
+        Console.WriteLine("NOW! (press enter after input)");
+
+        int tickCount = Environment.TickCount;
+        string input = Console.ReadLine();
+        int seconds = Environment.TickCount - tickCount;
+
+        Console.WriteLine();
+        Console.WriteLine("Din reaktionstid blev: " + seconds + " milisekunder.");
+        Console.ReadLine();
+    }
+}
